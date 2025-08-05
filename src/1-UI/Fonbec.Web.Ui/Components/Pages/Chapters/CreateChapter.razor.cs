25a912cef5feaf8095355f8c23192b77d5ef672f
luciopalmucci <lucio.palmucci@hotmail.com>
2025-08-05 13:32:55 -0300

    2 Created UI for "Create Chapter"

---
+++ src/1-UI/Fonbec.Web.Ui/Components/Pages/Chapters/CreateChapter.razor.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+
+namespace Fonbec.Web.Ui.Components.Pages.Chapters
+{
+    public partial class CreateChapter
+    {
+        private MudForm _form;
+        private string _nombre = string.Empty;
+
+        private void CrearFilial()
+        {
+            if (_form.IsValid) //Sino esta vacio
+            {
+                return; //Aca se llamaría al servicio para agregar el _nombre
+            }
+        }
+
+        private void Cancelar()
+        {
+            _nombre = string.Empty;
+        }
+    }
+}
