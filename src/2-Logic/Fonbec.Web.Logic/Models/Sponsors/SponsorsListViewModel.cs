e6471c2e66535822d545db07a7bf4e3a73549ddb
Reyna <reynamariapazceliz23@gmail.com>
2026-02-08 13:21:05 -0300

    37 Add GetAllSponsors to the Logic Layer
    Add method to the sponsor service
    Add create the corresponding List View Model

---
+++ src/2-Logic/Fonbec.Web.Logic/Models/Sponsors/SponsorsListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Fonbec.Web.DataAccess.DataModels.Sponsors;
+using Fonbec.Web.DataAccess.DataModels.Students;
+using Fonbec.Web.Logic.Models.Students;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fonbec.Web.Logic.Models.Sponsors
+{
+    public class SponsorsListViewModel : AuditableViewModel
+    {
+        public int SponsorId { get; set; } 
+
+        public string SponsorFirstName { get; set; } = string.Empty;
+
+        public string SponsorLastName { get; set; } = string.Empty;
+
+        public string SponsorNickName { get; set; } = string.Empty;
+    }
+
+    public class SponsorsListViewModelMappingDefinitions : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AllSponsorsDataModel, SponsorsListViewModel>()
+                .Map(dest => dest.SponsorId, src => src.SponsorId)
+                .Map(dest => dest.SponsorFirstName, src => src.SponsorFirstName)
+                .Map(dest => dest.SponsorLastName, src => src.SponsorLastName)
+                .Map(dest => dest.SponsorNickName, src => src.SponsorNickName ?? string.Empty);
+        }
+    }
+
+}
--- src/2-Logic/Fonbec.Web.Logic/Services/SponsorService.cs
+++ src/2-Logic/Fonbec.Web.Logic/Services/SponsorService.cs
@@ -1,6 +1,7 @@
 ﻿using Fonbec.Web.DataAccess.DataModels.Sponsors.Input;
 using Fonbec.Web.DataAccess.Repositories;
 using Fonbec.Web.Logic.Models.Results;
+using Fonbec.Web.Logic.Models.Sponsors;
 using Fonbec.Web.Logic.Models.Sponsors.Input;
 using Mapster;
 
@@ -8,10 +9,19 @@ namespace Fonbec.Web.Logic.Services;
 
 public interface ISponsorService
 {
+    // estoy agregando esto:
+    Task<List<SponsorsListViewModel>> GetAllSponsorsAsync();
     Task<CrudResult> CreateSponsorAsync(CreateSponsorInputModel createSponsorInputModel);
 }
 public class SponsorService(ISponsorRepository sponsorRepository) : ISponsorService
 {
+    public async Task<List<SponsorsListViewModel>> GetAllSponsorsAsync()
+    {
+        var allSponsorsDataModel = await sponsorRepository.GetAllSponsorsAsync();
+        // Adapt is not async...
+        var allSponsorsListViewModel = allSponsorsDataModel.Adapt<List<SponsorsListViewModel>>();
+        return allSponsorsListViewModel;
+    }
     public async Task<CrudResult> CreateSponsorAsync(CreateSponsorInputModel inputModel)
     {
         var inputDataModel = inputModel.Adapt<CreateSponsorInputDataModel>();
