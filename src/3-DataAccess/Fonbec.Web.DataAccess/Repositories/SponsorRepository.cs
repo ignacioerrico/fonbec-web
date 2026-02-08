940f0a549759fffddba5cc64409f40916797750a
Pedro Ignacio Errico <ignacio.errico@gmail.com>
2026-02-08 12:27:41 -0300

    37 Basic Data Access changes
    - Add method to get all sponsors to the Sponsor Repository class
    - Add corresponding data model

--- src/3-DataAccess/Fonbec.Web.DataAccess/Repositories/SponsorRepository.cs
+++ src/3-DataAccess/Fonbec.Web.DataAccess/Repositories/SponsorRepository.cs
@@ -1,16 +1,38 @@
 ﻿using Fonbec.Web.DataAccess.DataModels.Sponsors.Input;
 using Microsoft.EntityFrameworkCore;
 using Fonbec.Web.DataAccess.Entities;
+using Fonbec.Web.DataAccess.DataModels.Sponsors;
 
 namespace Fonbec.Web.DataAccess.Repositories;
 
 public interface ISponsorRepository
 {
+    Task<List<AllSponsorsDataModel>> GetAllSponsorsAsync();
     Task<int> CreateSponsorAsync(CreateSponsorInputDataModel dataModel);
 }
 
 public class SponsorRepository(IDbContextFactory<FonbecWebDbContext> dbContext) : ISponsorRepository
 {
+    public async Task<List<AllSponsorsDataModel>> GetAllSponsorsAsync()
+    {
+        await using var db = await dbContext.CreateDbContextAsync();
+
+        var allSponsors = await db.Sponsors
+            .Where(s => s.IsActive)
+            .Select(s => new AllSponsorsDataModel(s)
+            {
+                SponsorId = s.Id,
+                SponsorFirstName = s.FirstName,
+                SponsorLastName = s.LastName,
+                SponsorNickName = s.NickName,
+            })
+            .OrderBy(sdm => sdm.SponsorFirstName)
+            .ThenBy(sdm => sdm.SponsorLastName)
+            .ToListAsync();
+
+        return allSponsors;
+    }
+
     public async Task<int> CreateSponsorAsync(CreateSponsorInputDataModel dataModel)
     {
         await using var db = await dbContext.CreateDbContextAsync();
